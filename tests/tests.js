7132a03f75ef8363e8997d4f107da8635d42338b
victor-myha <victormyhaletsky@gmail.com>
2023-03-17 20:35:38 +0200

    Selenium search in Google

---
+++ tests/tests.js
@@ -0,0 +1,14 @@
+const { By, Key, Builder } = require("selenium-webdriver");
+require("chromedriver");
+
+const testCase = async () => {
+  let driver = await new Builder().forBrowser("chrome").build();
+
+  await driver.get("https://google.com");
+
+  await driver.findElement(By.name("q")).sendKeys("Hello, world", Key.RETURN);
+
+  setInterval(() => driver.quit(), 10000);
+};
+
+testCase();
